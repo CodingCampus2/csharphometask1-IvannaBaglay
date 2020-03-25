e647cca40d0708826242c6de6d72da51f3af5d12
IvannaB <ivankamorkai.baglay@gmail.com>
2020-03-25 15:12:26 +0200

    Fixed comments

--- Program.cs
+++ Program.cs
@@ -9,13 +9,19 @@ namespace HomeworkTemplate
         {
             Func<Task1, string> TaskSolver = task =>
             {
-                float numberOfSick = int.Parse(task.City.Population) * float.Parse(task.City.SickPercentage);
-                float numberOfKilledPeople = int.Parse(task.City.Population) * float.Parse(task.City.SickPercentage) * float.Parse(task.Virus.KillProbability);
+                if (float.TryParse(task.City.SickPercentage, out float sickPercentage)
+                    && float.TryParse(task.Virus.KillProbability, out float  killProbability)
+                    && int.TryParse(task.City.Population, out int population))
+                {
+                    float numberOfSick = population * sickPercentage;
+                    float numberOfKilledPeople = numberOfSick * killProbability;
 
-                double roundedNumberOfSick = Math.Truncate(numberOfSick);
-                double roundedNumberKilledPeople = Math.Truncate(numberOfKilledPeople);
+                    double roundedNumberOfSick = MathF.Truncate(numberOfSick);
+                    double roundedNumberKilledPeople = MathF.Truncate(numberOfKilledPeople);
 
-                return $"There are {roundedNumberOfSick} people sick with {task.Virus.Name} in the city of {task.City.Name}, {roundedNumberKilledPeople} of which died";
+                    return $"There are {roundedNumberOfSick} people sick with {task.Virus.Name} in the city of {task.City.Name}, {roundedNumberKilledPeople} of which died";
+                }
+                return "Error: Wrong data";
             };
             Task1.CheckSolver(TaskSolver);
         }
